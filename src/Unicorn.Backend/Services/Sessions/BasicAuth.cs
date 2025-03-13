f3a20a23aaa4f791876112ad2f174db84f7857bb
Vitaliy Dobriyan <dobriy88@yandex.ru>
2025-03-13 12:10:54 +0300

    added BasicAuth and BearerToken

---
+++ src/Unicorn.Backend/Services/Sessions/BasicAuth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace Unicorn.Backend.Services.Sessions
+{
+    /// <summary>
+    /// Represents Basic Auth mechanism. The token value is generated as Base64 of 'username:password'.
+    /// Before a request is sent the value is added to the request in Authorization header with value "Basic {token}"
+    /// </summary>
+    public class BasicAuth : IServiceSession
+    {
+        private readonly string _credentials;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicAuth"/> class with token username and password.<br/>
+        /// </summary>
+        /// <param name="username">username value</param>
+        /// <param name="password">password value</param>
+        public BasicAuth(string username, string password)
+        {
+            _credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+        }
+
+        /// <summary>
+        /// Updates instance of <see cref="HttpRequestMessage"/> with Authorization header with value "Basic {token}"
+        /// </summary>
+        /// <param name="request">service request instance</param>
+        public void UpdateRequestWithSessionData(HttpRequestMessage request) =>
+            request.Headers.Add("Authorization", "Basic " + _credentials);
+    }
+}
---
+++ src/Unicorn.Backend/Services/Sessions/BearerToken.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Unicorn.Backend.Services.Sessions
+{
+    /// <summary>
+    /// Represents Bearer token with expiration time. Before a request is sent the token is added to the request
+    /// in Authorization header with value "Bearer {token}"
+    /// </summary>
+    public class BearerToken : IServiceSession
+    {
+        private readonly string _token;
+        private readonly DateTime _expirationDate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BearerToken"/> class with token value and expiration time.<br/>
+        /// </summary>
+        /// <param name="token">token value</param>
+        /// <param name="expiresIn">expiration time as <see cref="TimeSpan"/></param>
+        public BearerToken(string token, TimeSpan expiresIn)
+        {
+            _token = token;
+            _expirationDate = DateTime.Now.Add(expiresIn);
+        }
+
+        /// <summary>
+        /// Indicates whether the token is expired or not
+        /// </summary>
+        public bool Expired => DateTime.Now > _expirationDate;
+
+        /// <summary>
+        /// Updates instance of <see cref="HttpRequestMessage"/> with Authorization header with value "Bearer {token}"
+        /// </summary>
+        /// <param name="request">service request instance</param>
+        public void UpdateRequestWithSessionData(HttpRequestMessage request) =>
+            request.Headers.Add("Authorization", "Bearer " + _token);
+    }
+}
